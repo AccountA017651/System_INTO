08ff7424d48ce96e56b3f3a8101cf031ef3f12ea
unknown <account_a_06257@proton.me>
2026-05-03 20:58:38 +0800

    Update 0.2.0.P

--- Core/Primary_Core/primary_core_0_1.cpp
+++ Core/Primary_Core/primary_core_0_1.cpp
@@ -2,10 +2,15 @@
 
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/color.hpp>
 
 #include "OS_Obtainer_Utility/os_obtainer_utility_0_1.hpp"
 
 int main() {
-    std::string os = Obtain_OS();
-    std::cout << os;
+    auto screen = ftxui::ScreenInteractive::TerminalOutput();
+
+    auto button = ftxui::Button("Quit", screen.ExitLoopClosure());
+
+    screen.Loop(button);
 }
